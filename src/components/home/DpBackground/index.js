75445ecb0bad3dd136c7cce6ba5e954a4782b819
pqhtobi <hung.phan_dp04@devplus.edu.vn>
2022-11-08 14:24:42 +0700

    implement-background/3.1

---
+++ src/components/home/DpBackground/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BtnBg, Container, ContentBg, Row, TitleBg } from "./style";
+import Slide from "react-reveal/Slide";
+
+const DpBackground = ({ titleBg, contentBg, btnBg }) => {
+  const openInNewTab = (url) => {
+    window.open(url, "_blank");
+  };
+  return (
+    <Container className="container-fluid">
+      <Row>
+        <Slide left>
+          <TitleBg>{titleBg}</TitleBg>
+        </Slide>
+        <Slide right>
+          <ContentBg>{contentBg}</ContentBg>
+        </Slide>
+        <Slide bottom>
+          <BtnBg
+            onClick={() =>
+              openInNewTab(
+                "https://stunited.typeform.com/registration?typeform-source=devplus.edu.vn"
+              )
+            }
+          >
+            {btnBg}
+          </BtnBg>
+        </Slide>
+      </Row>
+    </Container>
+  );
+};
+export default DpBackground;
